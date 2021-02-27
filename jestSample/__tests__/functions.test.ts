5241d72ce53b46ae29ef1f461a56703dd1121ecf
mana-000 <mana.hashimoto.1995@gmail.com>
2021-02-27 15:06:58 +0900

    10_functionsのtest

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,41 +1,134 @@
 // todo: ここに単体テストを書いてみましょう！
-import { sumOfArray, asyncSumOfArray } from "../functions";
+import { sumOfArray, asyncSumOfArray, asyncSumOfArraySometimesZero, getFirstNameThrowIfLong } from "../functions";
 import { NameApiService } from "../nameApiService";
+import { DatabaseMock } from "../util/index";
 
 jest.mock("../util/index");
 jest.mock("../nameApiService");
 
+const DatabaseMockMock = DatabaseMock as jest.Mock;
+const NameApiServiceMock = NameApiService as jest.Mock;
 
-// asyncSumOfArraySometimesZero,
-// getFirstNameThrowIfLong,
-// const sum1 = require('./functions.ts');
 
 describe('sumOfArray OK', () => {
   test('1+1=2', () => {
     expect(sumOfArray([1,1])).toBe(2);
   });
 });
-describe('sumOfArray empty NG', () => {
-  test('empty', () => {
-    expect(sumOfArray([])).toThrow('NGです');
-  });
-});
+// Failedになる
+// describe('sumOfArray empty NG', () => {
+//   test('empty', () => {
+//     expect(sumOfArray([])).toThrow('NGです');
+//   });
+// });
+
+// エラーで落ちる
 // describe('sumOfArray strinh NG', () => {
 //   test('string', () => {
 //     expect(sumOfArray(["abc"])).toThrow();
 //   });
 // });
+
 describe('asyncSumOfArray OK', () => {
   test('1+1=2', () => {
-    expect(asyncSumOfArray([1,1])).toBe(2);
+    return asyncSumOfArray([1,1]).then(data => {
+      expect(data).toBe(2);
+    });
   });
 });
-// describe('asyncSumOfArray OK', () => {
-//   test('OK', () => {
-//     var list: Array<number> = new Array(3);
-//     list[0] = [1, 1];
-//     list[1] = [2, 2];
-//     //let listB: Array<number> = {[1, 1],[2, 2]};
-//     expect(asyncSumOfArray(list)).toBe([2, 4]);
-//   });
-// });
+
+describe('asyncSumOfArraySometimesZero OK', () => {
+  test('hoge', () => {
+    const databaseMock = new DatabaseMock();
+    expect(DatabaseMockMock).toHaveBeenCalled();
+    return asyncSumOfArraySometimesZero([1,1], databaseMock).then(data => {
+      expect(data).toBe(2);
+    });
+  });
+});
+
+describe('asyncSumOfArraySometimesZero error', () => {
+  test('hoge', () => {
+    const databaseMock = new DatabaseMock();
+    expect(DatabaseMockMock).toHaveBeenCalled();
+    return asyncSumOfArraySometimesZero([], databaseMock).then(data => {
+      expect(data).toBe(0);
+    });
+  });
+});
+
+describe('getFirstNameThrowIfLong', () => {
+  test('OK', async () => {
+    const nameApiSerivceMock = new NameApiService();
+    //getFirstNameに名前を入れてgetFirstNameThrowIfLongを実行したい。。。
+    nameApiSerivceMock.getFirstName = ()  => {
+      // 実際はDBやAPIを使う複雑なもの
+      // promise.then((res) => {
+        return Promise.resolve('aaa');
+      // });
+    }
+    console.log(nameApiSerivceMock.getFirstName());
+    return getFirstNameThrowIfLong(10, nameApiSerivceMock).then(data => {
+      expect(data).toBe('aaa');
+    });
+    expect(NameApiServiceMock).toHaveBeenCalled();
+  });
+
+  test('LONGER', async () => {
+    const nameApiSerivceMock = new NameApiService();
+    nameApiSerivceMock.getFirstName = ()  => {
+      return Promise.resolve('aaa');
+    }
+    console.log(nameApiSerivceMock.getFirstName());
+    expect(getFirstNameThrowIfLong(2, nameApiSerivceMock)).rejects.toEqual(new Error('first_name too long'));
+  });
+});
+
+
+
+
+
+//以下雑メモ
+describe('getFirstNameThrowIfLong OK', () => {
+  // test('hoge', async () => {
+  // await expect(repeat('go', 5))
+  //   .resolves
+  //   .toBe('gogogogogo');
+  // });
+
+  test('OK', async () => {
+    const nameApiSerivceMock = new NameApiService();
+    //expect(NameApiServiceMock).toHaveBeenCalled();
+    // return getFirstNameThrowIfLong(4, nameApiSerivceMock).then(data => {
+    //   expect(data).toBe("aaa");
+    // });
+    // nameApiSerivceMock((): any => {
+    //     return {
+    //       getFirstName: (): string => {
+    //         return 'aaa'
+    //       }
+    //     }
+    // })
+    // async nameApiSerivceMock.getFirstName(): Promise<string> {
+    //   return 'foo';
+    // }
+    // nameApiSerivceMock.getFirstName: string => {
+    //   return 'aaa'
+    // }
+
+
+    // console.log(nameApiSerivceMock.getFirstName());
+    // return getFirstNameThrowIfLong(10, nameApiSerivceMock).then(data => {
+    //   expect(data).toBe('aaa');
+    // });
+    // expect(NameApiServiceMock).toHaveBeenCalled();
+    //expect(nameApiSerivceMock.getFirstName).toBe('aaa');
+    //expect(getFirstNameThrowIfLong(4, nameApiSerivceMock)).toBe('aaa');
+  });
+  // test('LONGER', async () => {
+    // return getFirstNameThrowIfLong(2, nameApiSerivceMock).then(data => {
+    //   // expect(data).toThrow("first_name too long");
+    //   expect(data).rejects.toEqual(new Error('first_name too long'));
+    // });
+  // });
+});
